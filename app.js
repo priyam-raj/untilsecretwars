56f081ff0611f29a8453f36ce441a83b74a4a8e4
Priyam Raj <hello@priyamraj.com>
2022-08-05 02:23:29 +0530

    Pilot

---
+++ app.js
@@ -0,0 +1,101 @@
+import express from "express";
+import { CronJob } from "cron";
+import dotenv from "dotenv";
+import { TwitterApi } from "twitter-api-v2";
+import { IgApiClient } from "instagram-private-api";
+import { promisify } from "util";
+import { buildDaysLeftImage } from "./assembleImage.js";
+import { readFile } from "fs";
+
+
+const app = express();
+const readFileAsync = promisify(readFile);
+dotenv.config();
+
+  app.use(express.static("public"));
+  app.listen(process.env.PORT || 3000, function () {
+  });
+
+  app.get("/", function (request, response) {
+    response.send('untilsecretwars is currently up and running <br><br> - Instagram @untilsecretwars<br> - Twitter @untilsecretwars');
+  });
+
+
+    // Set 3 second delay (For compressions and resizes.)
+function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  
+    
+  
+  async function tweetNow() {
+    console.log("Tweeting begins..");
+    let caption = await buildDaysLeftImage();
+    caption = `📅 ${caption} days left until #AvengersSecretWars.`;   
+  
+      const client = new TwitterApi({
+        appKey: process.env.CONSUMER_KEY,
+        appSecret: process.env.CONSUMER_SECRET,
+        accessToken: process.env.ACCESS_TOKEN,
+        accessSecret: process.env.ACCESS_TOKEN_SECRET,
+      });
+    
+    const untilsecretwars = client.readWrite;
+    const imagePath = `daysleft.jpg`
+  
+    const tweet = async () => {
+      try { 
+        const mediaId = await client.v1.uploadMedia(imagePath);
+        await untilsecretwars.v2.tweet(caption, {
+          media: { media_ids: [mediaId] },
+        });
+        console.log("Tweeted to Twitter!");
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    await tweet();
+  }
+  
+
+  async function instagramPost() {
+    try {
+      console.log("Posting to Instagram begins..");
+      // Instagram client
+      const { username, password } = process.env;
+      const ig = new IgApiClient();
+      let caption = await buildDaysLeftImage();
+      caption = `📅 ${caption} days left until Avengers: Secret Wars.\n\n#avengers #marvel #spiderman #avengerssecretwars #kangdynasty #kang #marvel #mcu #avengersendgame #captainamerica #thor #marvelcomics #blackwidow #hulk #marvelstudios #avengersinfinitywar #comics #secretwars #loki #marveluniverse #marvelcinematicmultiverse #marvelcinematicuniverse #avengersassemble`;
+      ig.state.generateDevice(username);
+      const user = await ig.account.login(username, password);
+      const path = `daysleft.jpg`;
+      const published = await ig.publish.photo({
+        file: await readFileAsync(path),
+        caption: caption,
+      });
+      console.log("Posted to Instagram!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  // Run on every day at 12 PM IST
+  let dailyPost = new CronJob(
+    "0 12 * * *",
+    function () {
+      console.log("Auto post to Instagram begins..");
+      instagramPost();
+      tweetNow();
+      },
+    true
+  );
+  
+  
+//   tweetNow();
+  instagramPost();
+  tweetNow();
+
+  
+  dailyPost.start();
+  console.log('Successfully started, now tweeting and posting to Instagram!');
+  
---
+++ assembleImage.js
@@ -0,0 +1,78 @@
+import fetch from "node-fetch";
+import Jimp from "jimp";
+
+// For adding manual delays. 
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function daysLeft() {
+
+  let secretWarsDate = "11/01/2025";
+
+  // Fetch Today's Date
+  let today = new Date().toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hours: "numeric",
+  });
+
+  let date1 = new Date(today);
+  let date2 = new Date(secretWarsDate);
+
+
+  let Difference_In_Time = date2.getTime() - date1.getTime();
+  let daysLeft = (Difference_In_Time / (1000 * 3600 * 24)).toString();
+
+  return daysLeft;
+}
+
+async function buildDaysLeftImage() {
+  let noOfDays = await daysLeft();
+
+  let daysText;
+
+  if (parseInt(noOfDays) > 1) {
+    daysText = "days"
+  } else { 
+    daysText = "day"
+  }
+
+  let font2 = await Jimp.loadFont(`assets/font/avengeance-80.fnt`);
+  let font1 = await Jimp.loadFont(`assets/font/avengeance-100.fnt`);
+
+
+  Jimp.read('assets/background.png', (err, image) => {
+    if (err) throw err;
+    Jimp.loadFont(`assets/font/avengeance-100.fnt`, (err, font1) => {
+      var w = image.bitmap.width;
+      var h = image.bitmap.height;
+      let text = noOfDays
+      var textWidth = Jimp.measureText(font1, text);
+      var textHeight = Jimp.measureTextHeight(font1, text);
+      var textWidth2 = Jimp.measureText(font2, daysText);
+      var textHeight2 = Jimp.measureTextHeight(font2, daysText);
+      image
+        .print(font1, w/2 - textWidth/2, h/2 - textHeight/2 - 120,
+          {   
+          text: text,
+          }, textWidth, textHeight)
+        .print(font2, w/2 - textWidth2/2, textHeight2/2 + 270,
+          {   
+          text: daysText,
+          }, textWidth2, textHeight2)
+        .write('daysleft.jpg'); // save
+    }); 
+  });
+  await timeout(1000);
+
+  return noOfDays;
+
+}
+
+
+buildDaysLeftImage()
+
+
+export { buildDaysLeftImage };
